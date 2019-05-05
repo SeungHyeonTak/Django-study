ad09196dcaa06979f4fcd43b4a8908e60e560e51
SeungHyeonTak <conficker77@gmail.com>
2019-05-05 22:16:33 +0900

    add_edit

--- Django_Inflearn01/mysite/models.py
+++ Django_Inflearn01/mysite/models.py
@@ -5,10 +5,10 @@ import random
 
 class GuessNumbers(models.Model): # Super_class (models) - Sub_class (Model) 상속
     name = models.CharField(max_length = 24)
-    lottos = models.CharField(max_length = 255,default = '[1,2,3,4,5,6]')
+    lottos = models.CharField(max_length = 255, default = '[1,2,3,4,5,6]')
     text = models.CharField(max_length = 255) # text는 한줄 메모장 처럼 간단하게
     num_lotto = models.IntegerField(default=5) # 게임 수
-    update_data = models.DateTimeField()
+    update_data = models.DateTimeField(auto_now=True)
 
     # Lotto 번호를 생성 및 데이터 베이스 저장
     def generate(self):
--- Django_Inflearn01/mysite/tests.py
+++ Django_Inflearn01/mysite/tests.py
@@ -3,10 +3,11 @@ from .models import GuessNumbers
 # Create your tests here.
 
 # test code 작성
-class GuessNumbersTestCase(TestCase):
+class GuessNumbersTestCase(TestCase): #testcase를 상속 받아온다.
     def test_generate(self):
-        g = GuessNumbers(name = 'apple', text = 'pineappne')
+        g = GuessNumbers(name = 'mark', text = '당첨?')
         g.generate()
-        print(g.update_data)
-        print(g.lottos)
-        self.assertTrue(len(g.lottos) > 20)
+        print(g.update_data) # 현재시간으로 업뎃
+        print(g.lottos) # Lotto의 내용
+        self.assertTrue(len(g.lottos) > 20) # 글자수가 20글자수보다 크면 통과한다.
+        # 성공인지 실패인지 확인할때 assertTrue로 확인
